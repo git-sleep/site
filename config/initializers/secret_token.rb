bb39f9ad33e6a142bf460c1840116d811577ae27
Max Jacobson <maxwell.jacobson@gmail.com>
2013-08-10 11:45:56 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GitSleep::Application.config.secret_token = '183bcde53c44b92a9bbed4ef57438a81da862f6e37f34e68d1712ab2a24c817916267b62dde1ae57d296c0a3f017b76bfa6c80837bbbe7f974fd6c4c221bd1f9'
